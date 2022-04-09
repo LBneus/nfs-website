7f0d35d1cc43537c5d7d34c8aa878c7452c36d81
lbneus <luka.buskivadze@vanderbilt.edu>
2022-04-09 16:10:31 -0500

    initialize project

---
+++ src/pages/team-page/team-page.pages.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+import "./team-page.styles.scss";
+
+const TeamPage = () => (
+
+    <div className='team-page-container'>
+        <h1>Hello</h1>
+    </div>
+
+)
+
+export default TeamPage;
